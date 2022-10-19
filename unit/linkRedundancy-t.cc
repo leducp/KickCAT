ca0dc6e3a2b08b7643db8d57586e97a1bb29f689
Theo <theo.martin@wandercraft.eu>
2022-10-19 15:48:35 +0200

    TU is redundancy needed

--- unit/linkRedundancy-t.cc
+++ unit/linkRedundancy-t.cc
@@ -1,7 +1,8 @@
 #include <gtest/gtest.h>
 #include <cstring>
-#include "Mocks.h"
+#include <string.h>
 
+#include "Mocks.h"
 #include "kickcat/LinkRedundancy.h"
 
 using ::testing::_;
@@ -286,3 +287,88 @@ TEST_F(LinkRedTest, writeThenRead_error_wrong_number_bytes_read)
 
     ASSERT_THROW(link.writeThenRead(frame), Error);
 }
+
+TEST_F(LinkRedTest, isRedundancyNeeded_true)
+{
+    EXPECT_CALL(*io_redundancy, write(_,_))
+    .WillOnce(Invoke([&](uint8_t const*, int32_t)
+    {
+        return ETH_MIN_SIZE;
+    }));
+
+    EXPECT_CALL(*io_nominal, read(_,_))
+    .WillOnce(Invoke([](uint8_t*, int32_t)
+    {
+        // The frame didn't reach the nominal interface.
+        return -1;
+    }));
+
+    EXPECT_CALL(*io_redundancy, read(_,_))
+    .WillOnce(Invoke([](uint8_t* frame_in, int32_t)
+    {
+        // Add a datagram to the read frame and set its working counter to 1.
+        Frame frame;
+        frame.addDatagram(0, Command::BRD,  0, nullptr, 1);
+        int32_t toWrite = frame.finalize();
+        std::memcpy(frame_in, frame.data(), toWrite);
+        uint8_t* datagram_addr = frame_in + sizeof(EthernetHeader) + sizeof(EthercatHeader);
+        DatagramHeader const* header = reinterpret_cast<DatagramHeader*>(datagram_addr);
+        uint8_t* wkc_addr = datagram_addr + sizeof(DatagramHeader) + header->len;
+        uint16_t wkc = 1;
+        std::memcpy(wkc_addr, &wkc, sizeof(uint16_t));
+        return toWrite;
+    }));
+
+    link.checkRedundancyNeeded();
+
+    ASSERT_EQ(is_redundancy_activated, true);
+}
+
+TEST_F(LinkRedTest, isRedundancyNeeded_false)
+{
+    EXPECT_CALL(*io_redundancy, write(_,_))
+    .WillOnce(Invoke([&](uint8_t const*, int32_t)
+    {
+        return ETH_MIN_SIZE;
+    }));
+
+    EXPECT_CALL(*io_nominal, read(_,_))
+    .WillOnce(Invoke([](uint8_t*, int32_t)
+    {
+        return ETH_MIN_SIZE;
+    }));
+
+    // The read frame has a working counter of 0
+    link.checkRedundancyNeeded();
+
+    ASSERT_EQ(is_redundancy_activated, false);
+}
+
+TEST_F(LinkRedTest, isRedundancyNeeded_no_interfaces)
+{
+    EXPECT_CALL(*io_redundancy, write(_,_))
+    .WillOnce(Invoke([&](uint8_t const*, int32_t)
+    {
+        return ETH_MIN_SIZE;
+    }));
+
+    EXPECT_CALL(*io_nominal, read(_,_))
+    .WillOnce(Invoke([](uint8_t*, int32_t)
+    {
+        return -1;
+    }));
+
+    EXPECT_CALL(*io_redundancy, read(_,_))
+    .WillOnce(Invoke([](uint8_t*, int32_t)
+    {
+        return -1;
+    }));
+
+    // The read frame has a working counter of 0
+    link.checkRedundancyNeeded();
+
+    ASSERT_EQ(is_redundancy_activated, false);
+}
+
+
+
