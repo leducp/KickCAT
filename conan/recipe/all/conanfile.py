e90238147ff70cebfd0c1532720ad9ce6037406b
ThomasNS <thomas.narayana-swamy@wandercraft.eu>
2023-08-03 17:07:34 +0200

    conan recipe update

--- conan/recipe/all/conanfile.py
+++ conan/recipe/all/conanfile.py
@@ -1,9 +1,13 @@
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 from conan.tools.files import get, copy
+from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.55"
+
 
 class KickCATRecipe(ConanFile):
     name = "kickcat"
@@ -12,30 +16,48 @@ class KickCATRecipe(ConanFile):
     description = "Thin EtherCAT stack designed to be embedded in a more complex software and with efficiency in mind"
     license = "CeCILL-C"
     topics = ("ethercat")
+    package_type = "library"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version])
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
+    def configure(self):
+        if self.options.get_safe("shared"):
+            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, 17)
+
+        if self.settings.os != "Linux":
+            raise ConanInvalidConfiguration(
+                f"{self.ref} is not supported on {self.settings.os}.")
+
+        if self.settings.compiler != "gcc":
+            raise ConanInvalidConfiguration(
+                f"{self.ref} is not supported on {self.settings.compiler}.")
+
+        if self.settings.compiler == 'gcc' and Version(self.settings.compiler.version) < "7":
+            raise ConanInvalidConfiguration("Building requires GCC >= 7")
+
     def generate(self):
         tc = CMakeToolchain(self)
+        tc.cache_variables["BUILD_UNIT_TESTS"] = "OFF"
+        tc.cache_variables["BUILD_EXAMPLES"] = "OFF"
+        tc.cache_variables["BUILD_SIMULATION"] = "OFF"
+        tc.cache_variables["BUILD_TOOLS"] = "OFF"
+        tc.cache_variables["DEBUG"] = "OFF"
         tc.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(variables={"BUILD_UNIT_TESTS": "OFF",
-                                   "BUILD_EXAMPLES": "OFF",
-                                   "BUILD_SIMULATION": "OFF",
-                                   "BUILD_TOOLS": "OFF"})
+        cmake.configure()
         cmake.build()
 
     def package(self):
@@ -45,6 +67,8 @@ class KickCATRecipe(ConanFile):
              os.path.join(self.package_folder, "lib"), keep_path=False)
         copy(self, "*.so", self.build_folder,
              os.path.join(self.package_folder, "lib"), keep_path=False)
+        copy(self, "LICENSE", self.source_folder,
+             os.path.join(self.package_folder, "licenses"))
 
     def package_info(self):
         self.cpp_info.libs = ["kickcat"]
