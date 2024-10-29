08968215d2a6a4208ac7cc531a6900f0e26bdde4
Nicolas Lemblé <nicolas.lemble@gmail.com>
2024-10-29 17:45:51 +0100

    unix instead of linux

--- lib/include/kickcat/types.h
+++ lib/include/kickcat/types.h
@@ -1,8 +1,8 @@
 #ifndef KICKCAT_TYPES_H
 #define KICKCAT_TYPES_H
 
-#if defined(__linux__) || defined(__NuttX__)
-#include "OS/Linux/types/os_types.h"
+#if defined(__unix__) 
+#include "OS/Unix/types/os_types.h"
 #elif __PikeOS__
 #include "OS/PikeOS/types/os_types.h"
 #endif
