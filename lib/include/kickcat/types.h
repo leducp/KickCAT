e266bcdad6298250498089fd065c7f6662f4479a
Nicolas Lemblé <nicolas.lemble@gmail.com>
2024-10-29 17:53:19 +0100

    try to go back to the __NuttX__ var

--- lib/include/kickcat/types.h
+++ lib/include/kickcat/types.h
@@ -1,7 +1,7 @@
 #ifndef KICKCAT_TYPES_H
 #define KICKCAT_TYPES_H
 
-#if defined(__unix__) 
+#if defined(__unix__) || defined(__NuttX__)
 #include "OS/Unix/types/os_types.h"
 #elif __PikeOS__
 #include "OS/PikeOS/types/os_types.h"
