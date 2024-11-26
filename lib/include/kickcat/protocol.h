39d5dbd83e46f621c30d6ebced92cb6673bce48b
Philippe Leduc <philippe.leduc@wandercraft.eu>
2024-11-26 10:54:07 +0100

    Separate CoE protocol in a dedicated header as it starts to grow a lot and others AL protocols will arrive in tyhe future (FoE, EoE)

--- lib/include/kickcat/protocol.h
+++ lib/include/kickcat/protocol.h
@@ -351,22 +351,22 @@ namespace kickcat
         uint8_t  pdi_control;
     } __attribute__((__packed__));
 
-    
+
     constexpr uint8_t SM_CONTROL_MODE_MASK = 0x03;
     constexpr uint8_t SM_CONTROL_MODE_BUFFERED = 0x00;
     constexpr uint8_t SM_CONTROL_MODE_MAILBOX = 0x02;
     constexpr uint8_t SM_CONTROL_DIRECTION_MASK = 0x0C;
     constexpr uint8_t SM_CONTROL_DIRECTION_READ = 0x00; // ECAT read access, PDI write access
     constexpr uint8_t SM_CONTROL_DIRECTION_WRITE = 0x04; // ECAT write access, PDI read access
-    constexpr uint8_t SM_CONTROL_INTERRUPT_ECAT_MASK = 0x10; 
-    constexpr uint8_t SM_CONTROL_INTERRUPT_ECAT_DISABLED = 0x00; 
-    constexpr uint8_t SM_CONTROL_INTERRUPT_ECAT_ENABLED = 0x10; 
-    constexpr uint8_t SM_CONTROL_INTERRUPT_AL_MASK = 0x20; 
-    constexpr uint8_t SM_CONTROL_INTERRUPT_AL_DISABLED = 0x00; 
-    constexpr uint8_t SM_CONTROL_INTERRUPT_AL_ENABLED = 0x20; 
-    constexpr uint8_t SM_CONTROL_WATCHDOG_MASK = 0x40; 
-    constexpr uint8_t SM_CONTROL_WATCHDOG_DISABLED = 0x00; 
-    constexpr uint8_t SM_CONTROL_WATCHDOG_ENABLED = 0x40; 
+    constexpr uint8_t SM_CONTROL_INTERRUPT_ECAT_MASK = 0x10;
+    constexpr uint8_t SM_CONTROL_INTERRUPT_ECAT_DISABLED = 0x00;
+    constexpr uint8_t SM_CONTROL_INTERRUPT_ECAT_ENABLED = 0x10;
+    constexpr uint8_t SM_CONTROL_INTERRUPT_AL_MASK = 0x20;
+    constexpr uint8_t SM_CONTROL_INTERRUPT_AL_DISABLED = 0x00;
+    constexpr uint8_t SM_CONTROL_INTERRUPT_AL_ENABLED = 0x20;
+    constexpr uint8_t SM_CONTROL_WATCHDOG_MASK = 0x40;
+    constexpr uint8_t SM_CONTROL_WATCHDOG_DISABLED = 0x00;
+    constexpr uint8_t SM_CONTROL_WATCHDOG_ENABLED = 0x40;
 
     enum SyncManagerType
     {
@@ -380,7 +380,7 @@ namespace kickcat
 
     constexpr uint8_t SYNC_MANAGER_CONTROL_OPERATION_MODE_MASK = (1 << 0);
     constexpr uint8_t SYNC_MANAGER_CONTROL_DIRECTION_MASK = (1 << 1);
-    
+
 
     char const* toString(SyncManagerType const& type);
 
@@ -627,135 +627,6 @@ namespace kickcat
         // Maximum simultaneous pending request in the gateway
         // Shall be a power of two and it shall not override with GATEWAY_MESSAGE_MASK
         constexpr uint16_t GATEWAY_MAX_REQUEST = 1024;
-    }
-
-    namespace CoE
-    {
-        constexpr uint16_t SM_COM_TYPE       = 0x1C00; // each sub-entry described SM[x] com type (mailbox in/out, PDO in/out, not used)
-        constexpr uint16_t SM_CHANNEL        = 0x1C10; // each entry is associated with the mapped PDOs (if in used)
-
-        struct Header
-        {
-            uint16_t number : 9,
-                    reserved : 3,
-                    service : 4; // i.e. request, response
-        } __attribute__((__packed__));
-        std::string toString(Header const* header); // Stringify the whole message, not just the header
-
-        struct ServiceData      // ETG1000.6 chapter 5.6.2 SDO
-        {
-            uint8_t size_indicator : 1,
-                    transfer_type : 1, // expedited or not
-                    block_size : 2,
-                    complete_access : 1,
-                    command : 3; // i.e. upload
-            uint16_t index;
-            uint8_t subindex;
-        } __attribute__((__packed__));
-
-        struct ServiceDataInfo // ETG1000.6 chapter 5.6.3 SDO Information
-        {
-            uint16_t opcode : 7,
-                     incomplete : 1,
-                     reserved : 8;
-            uint16_t fragments_left;
-        } __attribute__((__packed__));
-
-        struct Emergency        // ETG1000.6 chapter 5.6.4 Emergency
-        {
-            uint16_t error_code;
-            uint8_t  error_register;
-            uint8_t  data[5];
-        } __attribute__((__packed__));
-        std::string toString(Emergency const& emg);
-
-        enum Service
-        {
-            EMERGENCY            = 0x01,
-            SDO_REQUEST          = 0x02,
-            SDO_RESPONSE         = 0x03,
-            TxPDO                = 0x04,
-            RxPDO                = 0x05,
-            TxPDO_REMOTE_REQUEST = 0x06,
-            RxPDO_REMOTE_REQUEST = 0x07,
-            SDO_INFORMATION      = 0x08
-        };
-
-        namespace SDO
-        {
-            // Command specifiers depending on SDO request type
-            namespace request
-            {
-                constexpr uint8_t DOWNLOAD_SEGMENTED = 0x00;
-                constexpr uint8_t DOWNLOAD           = 0x01;
-                constexpr uint8_t UPLOAD             = 0x02;
-                constexpr uint8_t UPLOAD_SEGMENTED   = 0x03;
-                constexpr uint8_t ABORT              = 0x04;
-
-                char const* toString(uint8_t command);
-            }
-
-            namespace response
-            {
-                constexpr uint8_t UPLOAD_SEGMENTED    = 0x00;
-                constexpr uint8_t DOWNLOAD_SEGMENTED  = 0x01;
-                constexpr uint8_t UPLOAD              = 0x02;
-                constexpr uint8_t DOWNLOAD            = 0x03;
-            }
-
-            namespace information
-            {
-                constexpr uint8_t GET_OD_LIST_REQ    = 0x01;
-                constexpr uint8_t GET_OD_LIST_RESP   = 0x02;
-                constexpr uint8_t GET_OD_REQ         = 0x03;
-                constexpr uint8_t GET_OD_RESP        = 0x04;
-                constexpr uint8_t GET_ED_LIST_REQ    = 0x05;
-                constexpr uint8_t GET_ED_LIST_RESP   = 0x06;
-                constexpr uint8_t SDO_INFO_ERROR_REQ = 0x07;
-            }
-
-            namespace abort
-            {
-                constexpr uint32_t TOGGLE_BIT_NOT_ALTERNATED    = 0x05030000;
-                constexpr uint32_t SDO_PROTOCOL_TIMEOUT         = 0x05040000;
-                constexpr uint32_t COMMAND_SPECIFIER_INVALID    = 0x05040001;
-                constexpr uint32_t INVALID_BLOCK_SIZE           = 0x05040002;
-                constexpr uint32_t INVALID_SEQUENCE_NUMBER      = 0x05040003;
-                constexpr uint32_t CRC_ERROR                    = 0x05040004;
-                constexpr uint32_t OUT_OF_MEMORY                = 0x05040005;
-                constexpr uint32_t UNSUPPORTED_ACCESS           = 0x06010000;
-                constexpr uint32_t READ_ONLY_ACCESS             = 0x06010001;
-                constexpr uint32_t WRITE_ONLY_ACCESS            = 0x06010002;
-                constexpr uint32_t SUBINDEX0_CANNOT_BE_WRITTEN  = 0x06010003;
-                constexpr uint32_t COMPLETE_ACCESS_UNSUPPORTED  = 0x06010004;
-                constexpr uint32_t OBJECT_TOO_BIG               = 0x06010005;
-                constexpr uint32_t OBJECT_MAPPED                = 0x06010006;
-                constexpr uint32_t OBJECT_DOES_NOT_EXIST        = 0x06020000;
-                constexpr uint32_t OBJECT_CANNOT_BE_MAPPED      = 0x06040041;
-                constexpr uint32_t PDO_LENGTH_EXCEEDED          = 0x06040042;
-                constexpr uint32_t PARAMETER_INCOMPATIBILITY    = 0x06040043;
-                constexpr uint32_t INTERNAL_INCOMPATIBILITY     = 0x06040047;
-                constexpr uint32_t HARDWARE_ERROR               = 0x06060000;
-                constexpr uint32_t DATA_TYPE_LENGTH_MISMATCH    = 0x06070010;
-                constexpr uint32_t DATA_TYPE_LENGTH_TOO_HIGH    = 0x06070012;
-                constexpr uint32_t DATA_TYPE_LENGTH_TOO_LOW     = 0x06070013;
-                constexpr uint32_t SUBINDEX_DOES_NOT_EXIST      = 0x06090011;
-                constexpr uint32_t VALUE_RANGE_EXCEEDED         = 0x06090030;
-                constexpr uint32_t VALUE_TOO_HIGH               = 0x06090031;
-                constexpr uint32_t VALUE_TOO_LOW                = 0x06090032;
-                constexpr uint32_t MODULE_LIST_MISMATCH         = 0x06090033;
-                constexpr uint32_t MAX_LESS_THAN_MIN            = 0x06090036;
-                constexpr uint32_t RESSOURCE_UNAVAILABLE        = 0x060A0023;
-                constexpr uint32_t GENERAL_ERROR                = 0x08000000;
-                constexpr uint32_t TRANSFER_ABORTED_GENERIC     = 0x08000020;
-                constexpr uint32_t TRANSFER_ABORTED_LOCAL_CTRL  = 0x08000021;
-                constexpr uint32_t TRANSFER_ABORTED_ESM_STATE   = 0x08000022;
-                constexpr uint32_t DICTIONARY_GENERTION_FAILURE = 0x08000023;
-                constexpr uint32_t NO_DATA_AVAILABLE            = 0x08000024;
-            }
-
-            char const* abort_to_str(uint32_t abort_code);
-        }
     }
 
     // MAC addresses are not used by EtherCAT but set them helps the debug easier when following a network trace.
--- unit/protocol-t.cc
+++ unit/protocol-t.cc
@@ -5,27 +5,6 @@ using namespace kickcat;
 
 // no real logic here, just check that code to string functions returns valid result (right formed C string with a few caracters)
 
-TEST(Protocol, SDO_abort_to_str)
-{
-    for (uint32_t i = 0x05000000; i < 0x05050000; ++i)
-    {
-        char const* text = CoE::SDO::abort_to_str(i);
-        ASSERT_EQ(5, strnlen(text, 5));
-    }
-
-    for (uint32_t i = 0x06000000; i < 0x060A0000; ++i)
-    {
-        char const* text = CoE::SDO::abort_to_str(i);
-        ASSERT_EQ(5, strnlen(text, 5));
-    }
-
-    for (uint32_t i = 0x08000000; i < 0x08001000; ++i)
-    {
-        char const* text = CoE::SDO::abort_to_str(i);
-        ASSERT_EQ(5, strnlen(text, 5));
-    }
-}
-
 TEST(Protocol, ALStatus_to_string)
 {
     for (int32_t i = 0; i < UINT16_MAX; ++i) // AL status code is defined on 16bits
