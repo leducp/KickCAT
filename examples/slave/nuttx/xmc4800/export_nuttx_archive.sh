be561e9e3b9bfa5cd0fed8ef0aa2f07619972c86
Johnathan Caselles <johnathan.caselles@wandercraft.health>
2025-03-24 13:44:19 +0100

    adapted <make export -j> depending on the available cores of user's pc

--- examples/slave/nuttx/xmc4800/export_nuttx_archive.sh
+++ examples/slave/nuttx/xmc4800/export_nuttx_archive.sh
@@ -69,7 +69,6 @@ cd nuttx
 ## NOTE
 
 ## 1.3.4: Build NuttX with make export (building with 'make' may show an error about an "undefined reference to 'main'")
-#make export -j2
 make export -j$(($(nproc) - 2))
 
 # The export process generates a compressed file (e.g., nuttx-export.tar.gz) in the nuttx folder.
