8b71d2098a775b37c251bcc68ab7cc225ba85a51
trns1997 <trns1997@gmail.com>
2025-09-12 13:31:58 +0200

    rework spi interface

--- lib/slave/include/kickcat/AbstractSPI.h
+++ lib/slave/include/kickcat/AbstractSPI.h
@@ -1,8 +1,8 @@
-
 #ifndef SLAVE_STACK_INCLUDE_ABSTRACTSPI_H_
 #define SLAVE_STACK_INCLUDE_ABSTRACTSPI_H_
 
 #include <cstdint>
+#include <string>
 
 namespace kickcat
 {
@@ -12,13 +12,26 @@ namespace kickcat
         virtual ~AbstractSPI() = default;
 
         virtual void init() = 0;
-        void read( void* data, uint32_t size);
+
+        void read(void* data, uint32_t size);
         void write(void const* data, uint32_t size);
         virtual void transfer(uint8_t const* data_write, uint8_t* data_read, uint32_t size) = 0;
 
         virtual void enableChipSelect() = 0;
         virtual void disableChipSelect() = 0;
-    };
 
+        virtual void setBaudRate(uint32_t baud) = 0;
+        virtual uint32_t getBaudRate() const = 0;
+
+        virtual void setMode(uint8_t mode) = 0;
+        virtual uint8_t getMode() const = 0;
+
+        virtual void setDevice(const std::string& devicePath) = 0;
+        virtual std::string getDevice() const = 0;
+
+        virtual void setChipSelect(uint32_t cs) = 0;
+        virtual uint32_t getChipSelect() const = 0;
+    };
 }
+
 #endif
--- unit/mocks/SPI.h
+++ unit/mocks/SPI.h
@@ -2,7 +2,6 @@
 #define KICKCAT_UNIT_MOCKS_SPI_H
 
 #include <gmock/gmock.h>
-
 #include "kickcat/AbstractSPI.h"
 
 namespace kickcat
@@ -14,9 +13,21 @@ namespace kickcat
         virtual ~MockSPI() = default;
 
         MOCK_METHOD(void, init, (), (override));
-        MOCK_METHOD(void, transfer,  (uint8_t const*, uint8_t*, uint32_t), (override));
+        MOCK_METHOD(void, transfer, (uint8_t const*, uint8_t*, uint32_t), (override));
         MOCK_METHOD(void, enableChipSelect, (), (override));
         MOCK_METHOD(void, disableChipSelect, (), (override));
+
+        MOCK_METHOD(void, setBaudRate, (uint32_t baud), (override));
+        MOCK_METHOD(uint32_t, getBaudRate, (), (const, override));
+
+        MOCK_METHOD(void, setMode, (uint8_t mode), (override));
+        MOCK_METHOD(uint8_t, getMode, (), (const, override));
+
+        MOCK_METHOD(void, setDevice, (const std::string& devicePath), (override));
+        MOCK_METHOD(std::string, getDevice, (), (const, override));
+
+        MOCK_METHOD(void, setChipSelect, (uint32_t cs), (override));
+        MOCK_METHOD(uint32_t, getChipSelect, (), (const, override));
     };
 }
 
