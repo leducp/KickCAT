f4f2eafea8cb2f23604e795d63f4804be517c79b
Philippe Leduc <philippe.leduc@genesis-ai.company>
2025-12-29 13:43:04 +0100

    Working state - all axis

--- lib/master/include/kickcat/Diagnostics.h
+++ lib/master/include/kickcat/Diagnostics.h
@@ -2,12 +2,17 @@
 #define KICKCAT_DIAGNOSTICS_H
 
 #include "kickcat/Slave.h"
+#include <unordered_map>
 
 namespace kickcat
 {
     /// \brief return the topology of discovered network - To be called after bus.getDLStatus()
     /// \return [key, value] pair : [slave adress, parent address] (the only slave that is its own parent is linked to the master)
     std::unordered_map<uint16_t, uint16_t> getTopology(std::vector<Slave>& slaves);
+
+    /// \brief Detect network topology using BFS traversal - To be called after DL status is fetched for all slaves
+    /// \return Map of [slave address, parent address] pairs. The first slave has itself as parent (connected to master)
+    std::unordered_map<uint16_t, uint16_t> detectTopology(std::vector<Slave>& slaves);
 }
 
 #endif
