2822f0bac05bebbb5a3f8062340415773468ba69
Philippe Leduc <philippe.leduc@genesis-ai.company>
2026-01-05 11:54:48 +0100

    Remove detectTopology(): not working and redundant.

--- lib/master/include/kickcat/Diagnostics.h
+++ lib/master/include/kickcat/Diagnostics.h
@@ -12,10 +12,6 @@ namespace kickcat
     /// \return [key, value] pair : [slave adress, parent address] (the only slave that is its own parent is linked to the master)
     std::unordered_map<uint16_t, uint16_t> getTopology(std::vector<Slave>& slaves);
 
-    /// \brief Detect network topology using BFS traversal - To be called after DL status is fetched for all slaves
-    /// \return Map of [slave address, parent address] pairs. The first slave has itself as parent (connected to master)
-    std::unordered_map<uint16_t, uint16_t> detectTopology(std::vector<Slave>& slaves);
-
     template<typename T>
     void read_address(Slave& slave, uint16_t address, T& value, std::shared_ptr<Link> link)
     {
